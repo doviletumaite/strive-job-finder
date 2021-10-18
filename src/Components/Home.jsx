135950d9167b639fc56e8f3189c09a97daac4b07
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-18 14:37:07 +0200

    up

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import {Col, Row, Form} from "react-bootstrap"
+
+const Home = () => {
+return (
+    <di>
+        <h1 className="text-center m-3">Strive Job Finder</h1>
+        <Form>
+  <Row>
+    <Col>
+      <Form.Control placeholder="Search" />
+    </Col>
+  </Row>
+</Form>
+    </di>
+)
+}
+
+export default Home
