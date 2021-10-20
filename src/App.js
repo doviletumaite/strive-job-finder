37c7c711ab881e4c4dd91d4cf9901fb9e055342a
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-20 15:53:49 +0200

    up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Home from './Components/Home';
+import singleJobPage from './Components/singleJobPage';
 
 function App() {
   return (
    <Router>
      <Route path="/" exact component={Home}/>
-     <Route exact path="/companyName" />
+     <Route exact path="/companyName" exact component={singleJobPage} />
    </Router>
   );
 }
---
+++ src/Components/singleJobPage.jsx
@@ -0,0 +1,8 @@
+const singleJobPage = ({jobs}) => {
+    console.log("job in detail page",jobs)
+return (
+    <div>{jobs.title}</div>
+)
+}
+
+export default singleJobPage
