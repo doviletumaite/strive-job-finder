4c6e5665f442f3ade902c6a8e0c45b634fb15b1a
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-23 09:59:47 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,5 +5,5 @@ import thunk from 'redux-thunk'
  
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const configureStore = createStore(mainReducer, initialState)
+const configureStore = createStore(mainReducer, initialState,composeEnhancers(applyMiddleware(thunk)))
 export default configureStore
